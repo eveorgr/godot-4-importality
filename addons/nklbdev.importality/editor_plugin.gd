e2604570131c85d91115325d17e54dd7008e6010
Evelyn Lima <104331636+EvelynLimaB@users.noreply.github.com>
2026-08-19 18:33:08 -0300

    fix: correct standalone loader array typing

--- addons/nklbdev.importality/editor_plugin.gd
+++ addons/nklbdev.importality/editor_plugin.gd
@@ -23,7 +23,7 @@ const IMPORTERS_SCRIPTS: Array[GDScript] = [
 ]
 
 const StandaloneImageFormatLoaderExtension = preload("standalone_image_format_loader_extension.gd")
-const STANDALONE_IMAGE_FORMAT_LOADER_EXTENSIONS: Array[GDScript> = [
+const STANDALONE_IMAGE_FORMAT_LOADER_EXTENSIONS: Array[GDScript] = [
 	preload("command_line_image_format_loader_extension.gd")
 ]
 
